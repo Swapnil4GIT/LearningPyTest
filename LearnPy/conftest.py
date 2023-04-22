548a7458ef7b52302dcb5332275efc6c1b97bc9d
Swapnil4GIT <100533909+Swapnil4GIT@users.noreply.github.com>
2023-04-22 20:30:20 +0530

    generate parameterized pytest

--- LearnPy/conftest.py
+++ LearnPy/conftest.py
@@ -1,11 +1,24 @@
 import pytest
 
 @pytest.fixture(scope='function', autouse=True) 
-def tc_setup():
-    print("Launch browser")
+def tc_setup(browser):
+
+    if browser == "chrome":
+        print("Launch chrome")
+    elif browser == "ff":
+        print("Launch firefox")
+    else:
+        print("Provide valid browser")
+
     print("Login")
     print("Browse products")
     yield 
     print("Logoff")
     print("Close browser")
 
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+@pytest.fixture(scope='function', autouse=True)
+def browser(request):
+    return  request.config.getoption('--browser')
